8645a3a4e13ae48ac85cd14d82a4cee07face329
boronji <nakaura01@hotmail.com>
2013-06-25 18:41:16 +0900

    remove neadless line

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
 #include <string>
